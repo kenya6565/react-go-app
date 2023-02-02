ca6d0d6211816d9c02d8835c3f8b5ea7e94ae145
kenya6565 <ken.australia0605@gmail.com>
2023-02-03 02:13:53 +0900

    add readJSON()

--- go-movies-back-end-master/cmd/api/utils.go
+++ go-movies-back-end-master/cmd/api/utils.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 )
 
@@ -31,4 +33,27 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 
 	return nil
 
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1024 * 1024 // one megabyte
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	//do not receive json that I do not know
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	// do not receive more than one json body at one request
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
+	return nil
 }
